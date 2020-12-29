66c9d92b70e65351e89a20b100a1410b3a526453
Jocelyn <72670056+tornozelo@users.noreply.github.com>
2020-12-29 08:28:51 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
+      <div class="container">
+      <h1>Weather App</h1>
+           <footer>
         This project was coded by Jocelyn Allen and is{" "}
       <a href="https://github.com/jocelyn-allen/react-weather-app-project" target="_blank">open-sourced on GitHub
 
       </a>
       .
       </footer>
+   </div>
    </div>
   );
 }
