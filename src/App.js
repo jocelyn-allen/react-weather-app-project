5954dcef8d1aa0659055a91c71cfb7f05b6802fa
Jocelyn <72670056+tornozelo@users.noreply.github.com>
2020-12-30 20:33:31 +0000

    Added API integration

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div class="container">
-            <Weather />
+            <Weather defaultCity="Liverpool" />
            <footer>
         This project was coded by Jocelyn Allen and is{" "}
       <a href="https://github.com/jocelyn-allen/react-weather-app-project" target="_blank" rel="noreferrer">open-sourced on GitHub
--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-    return <div className="Weather">
+export default function Weather(props) {
+    const [weatherData, setWeatherData] = useState({ ready: false });
+    function handleResponse(response) {
+        console.log(response.data);
+        setWeatherData({
+        ready: true,
+        temperature: response.data.main.temp,
+        humidity: response.data.main.humidity,
+        date: "Tuesday 08:00",
+        description: response.data.weather[0].description,
+        iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+        wind: response.data.wind.speed,
+        city: response.data.name,
+        })
+    }
+
+    if (weatherData.ready) {
+
+    return (
+    <div className="Weather">
         <form>
             <div className="row">
                 <div className="col-9">
@@ -14,17 +33,17 @@ export default function Weather() {
  </div>
  </div>
  </form>
-        <h1>Birmingham</h1>
+        <h1>{weatherData.city}</h1>
         <ul>
-<li>Tuesday 08:00</li>
-<li>Mostly cloudy</li>
+<li>{weatherData.date}</li>
+<li className="text-capitalize">{weatherData.description}</li>
         </ul>
         <div className="row mt-3">
 <div className="col-6">
      <div className="clearfix">
-<img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Mostly Cloudy" className="float-left" />
+<img src={weatherData.iconUrl} alt={weatherData.description} className="float-left" />
 <div className="float-left">
-<span className="temperature">1</span>
+<span className="temperature">{Math.round(weatherData.temperature)}</span>
 <span className="unit">°C</span>
 </div>
 </div>
@@ -32,17 +51,23 @@ export default function Weather() {
 <div className="col-6">
 <ul>
     <li>
-        Precipitation: 17%
+        Humidity: {weatherData.humidity}%
     </li>
     <li>
-        Humidity: 94%
-    </li>
-    <li>
-        Wind: 6 mph
+        Wind: {Math.round(weatherData.wind)} mph
     </li>
 </ul>
 
 </div>
         </div>
         </div>
+    );
+} else {
+const apiKey = "7f0e26ebcc0056eeecaacb8107edefb1";
+const units = "metric";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=${units}`
+axios.get(apiUrl).then(handleResponse);
+
+return "Loading...";
+}
 }
